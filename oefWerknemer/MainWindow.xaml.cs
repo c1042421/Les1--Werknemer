e0101e8e260505c15a9b1c4c2219963669d8d650
c1042421 <c1042421@hik.be>
2017-09-19 11:19:27 +0200

    checking input and showing output

--- oefWerknemer/MainWindow.xaml.cs
+++ oefWerknemer/MainWindow.xaml.cs
@@ -27,7 +27,32 @@ namespace oefWerknemer
 
         private void btnToevoegen_Click(object sender, RoutedEventArgs e)
         {
+            double verdiensten;
+            string voornaam = txtVoornaam.Text;
+            string achternaam = txtAchternaam.Text;
 
+            if (double.TryParse(txtVerdiensten.Text, out verdiensten) && voornaam.Length != 0 && achternaam.Length != 0)
+            {
+                txtOutput.Text += string.Format("{0} {1} {2}€\n", achternaam.PadRight(20), voornaam.PadRight(20), verdiensten.ToString().PadLeft(20));
+            } else
+            {
+                if (achternaam.Length == 0)
+                {
+                    toonMessageBox("Geef voornaam in!");
+                }
+                else if(voornaam.Length == 0)
+                {
+                    toonMessageBox("Geef een voornaam in!");
+                } else
+                {
+                    toonMessageBox("Geef een numerieke waarde in bij verdiensten!");
+                }
+            }
+        }
+
+        private void toonMessageBox(string message)
+        {
+            MessageBox.Show(message, "Fout!", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
